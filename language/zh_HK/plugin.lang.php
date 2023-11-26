f75cff30887033eb61568f1f1d06cd45e1537aeb
klaoun <36087587+klaoun@users.noreply.github.com>
2023-11-26 07:00:29 +0100

    Create plugin.lang.php

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos are missing in Piwigo: %s'] = 'Piwigo 中缺少 %d 張照片：%s';
+$lang['%d photos updated'] = '%d 張照片已更新';
+$lang['Description for another photo'] = '另一張照片的描述';
+$lang['File pattern'] = '檔案模式';
+$lang['Photo description, until the end of the line'] = '照片描述（直到行尾）';
+$lang['Send File - Photo Properties Mass Update'] = '下載檔案 - 照片屬性批次更新';
+$lang['Text file'] = '文字檔';
+$lang['Update'] = '更新';
+$lang['Wrong file, please select a plain text file'] = '檔案類型錯誤，請選擇純文字檔案';
+$lang['tab'] = 'tabulation';
+$lang['space'] = 'espace';
+$lang['comma'] = '逗號';
+$lang['semicolon'] = '分號';
+$lang['(comma separated)'] = '(逗號分隔)';
+?>
